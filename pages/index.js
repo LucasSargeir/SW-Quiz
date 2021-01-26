ed061acff7cf50180b8170d0821abfdd1d54ad6e
lucassargeir <sargeirolucas@gmail.com>
2021-01-26 20:04:52 -0300

    Subindo

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,33 @@
 import styled from 'styled-components'
+import db from '../db.json'
+import Widget from '../src/components/Widget'
+import QuizBackground from '../src/components/QuizBackground'
+import Footer from '../src/components/Footer'
+import GitHubCorner from '../src/components/GitHubCorner'
+import QuizContainer from '../src/components/QuizContainer'
 
-const Title = styled.h1`
-  font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
-`
 
 export default function Home() {
-  return <Title>My page</Title>
+  return(
+    <QuizBackground backgroundImage={db.bg}>
+      <QuizContainer>
+        <Widget>
+          <Widget.Header>
+            <h1>Star Wars</h1>
+          </Widget.Header>
+          <Widget.Content>
+            <p> Lorem ipsum dolor sit amet...</p>
+          </Widget.Content>
+        </Widget>
+        <Widget>
+          <Widget.Content>
+            <h1>Quiz da Galera</h1>
+            <p> Lorem ipsum dolor sit amet...</p>
+          </Widget.Content>          
+        </Widget>
+        <Footer/>
+      </QuizContainer>
+      <GitHubCorner projectUrl="https://github.com/LucasSargeir/sw-quiz"/>
+    </QuizBackground>
+  );
 }
